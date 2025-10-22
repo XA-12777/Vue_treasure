244de14da3cbf31593cd1125a574ad0f99a6da77
XA-12777 <3175237429@qq.com>
2025-10-22 19:14:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+
 export default defineConfig({
   plugins: [vue()],
   base: '/Vue_treasure/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    // 确保根目录文件被正确处理
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
