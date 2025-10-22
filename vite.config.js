d5b595ebc89bcad6be70df84024789e5f37ee8a9
XA-12777 <3175237429@qq.com>
2025-10-22 15:38:39 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/treasure-adventure/',  // ← 添加这一行，替换为你的仓库名
+  base: '/Vue_treasure/',  // ← 添加这一行，替换为你的仓库名
   build: {
     outDir: 'dist',              // ← 添加构建配置
     assetsDir: 'assets'
